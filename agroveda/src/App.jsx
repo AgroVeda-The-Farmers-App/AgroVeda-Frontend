37fd610674ce1a3b8fd196fa76032f5bbe39e8b4
Archan47 <chowdhuryarchan76@gmail.com>
2026-05-08 16:10:17 +0530

    updated app.jsx

--- agroveda/src/App.jsx
+++ agroveda/src/App.jsx
@@ -1,16 +1,22 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Home from "./components/Home";
 import Login from "./components/Login";
 import Signup from "./components/Signup";
+import ForgotPassword from "./components/ForgotPassword";
+import CropCalendar from "./components/Cropcalendar";
+
 import "./App.css";
-import Home from "./components/Home";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
+        <Route path="/"                element={<Home />} />
+        <Route path="/login"           element={<Login />} />
+        <Route path="/signup"          element={<Signup />} />
+        <Route path="/forgot-password" element={<ForgotPassword />} />
+        <Route path="/crop-calendar" element={<CropCalendar />} />
+  
       </Routes>
     </BrowserRouter>
   );
