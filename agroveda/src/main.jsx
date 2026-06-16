ad20f0b8a3efbddd89f7b739e4877411067e5c67
Archan47 <chowdhuryarchan76@gmail.com>
2026-06-16 09:58:20 +0530

    updated

--- agroveda/src/main.jsx
+++ agroveda/src/main.jsx
@@ -1,3 +1,25 @@
+// --- Google Translate / removeChild crash fix ---
+if (typeof Node === 'function' && Node.prototype) {
+  const originalRemoveChild = Node.prototype.removeChild;
+  Node.prototype.removeChild = function (child) {
+    if (child.parentNode !== this) {
+      console.warn('Cannot remove a child from a different parent', child, this);
+      return child;
+    }
+    return originalRemoveChild.apply(this, arguments);
+  };
+
+  const originalInsertBefore = Node.prototype.insertBefore;
+  Node.prototype.insertBefore = function (newNode, referenceNode) {
+    if (referenceNode && referenceNode.parentNode !== this) {
+      console.warn('Cannot insert before a reference node from a different parent', referenceNode, this);
+      return newNode;
+    }
+    return originalInsertBefore.apply(this, arguments);
+  };
+}
+// --- end fix ---
+
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
